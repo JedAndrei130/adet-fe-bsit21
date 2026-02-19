0ce07b5b4d4e83710d404e594af33543d5371d69
Jed Surabasquez <jasurabasquez66130@liceo.edu.ph>
2026-02-19 14:19:52 +0800

    update

--- src/app/posts.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-posts',
-  standalone: true,
-  templateUrl: './posts.component.html',
-  styleUrls: ['./posts.component.scss']
-})
-export class PostsComponent {}
